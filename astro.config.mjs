a888c70b7d2bf236528975756afcaf9148e769cb
Aaron Jordan <hello@aaronjordan.dev>
2025-09-01 16:22:36 -0700

    configure astro for cloudflare deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,22 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
+
   markdown: {
     shikiConfig: {
       theme: "kanagawa-wave",
       wrap: false,
     },
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
